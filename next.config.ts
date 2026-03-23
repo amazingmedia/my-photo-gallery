7d164efbf0261ad4be1a6e67667fbce9f779f717
amazingmedia <131604200+amazingmedia@users.noreply.github.com>
2026-03-23 19:32:09 +0630

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* လိုအပ်သော config များကို ဤနေရာတွင် ထည့်နိုင်ပါသည် */
 };
 
 export default nextConfig;
